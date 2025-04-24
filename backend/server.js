da6502e6c7411433a7519cc0801640c650d46e8b
timothychemweno254 <timothy.kiprop@student.moringaschool.com>
2025-04-24 08:56:21 +0300

    vercel

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,15 @@ const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); // Allow all origins
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', '*');
+  next();
+});
+
 server.use(middlewares);
-server.use('/api', router); // This sets the base route to /api
-server.listen(5001, () => {
+server.use('/Api', router);
+server.listen(process.env.PORT || 3000, () => {
   console.log('JSON Server is running');
 });
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+  server: {
+    proxy: {
+      '/Api': {
+        target: 'https://urbancars-app2.onrender.com',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+});
